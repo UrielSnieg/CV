962def65ef600e67d43cab3057af9a341affef94
unknown <urielsnieg06@gmail.com>
2023-01-31 19:07:06 -0300

    Proyecto CV terminado

---
+++ app.js
@@ -0,0 +1,7 @@
+
+document.getElementById('miboton').onclick = function () {
+    document.getElementById("demo").innerHTML = "jesus.graham@example.com"
+}
+
+
+// No me dio tiempo para implementar mas 
